5c138d60c7cc673756f4ca2129a1fa7bb95eeebc
Hafizh Fadhlurrohman <hafizh@infradigital.io>
2022-01-25 15:57:23 +0700

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/test_storage/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.test_storage
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,154 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var controller = TextEditingController();
+  var key = GlobalKey<FormState>();
+  String text = "A";
+
+  Future<void> readSingleFile() async {
+    try {
+      String? contents;
+      await Permission.storage.request();
+      if (await Permission.storage.request().isGranted) {
+        FilePickerResult? selectedDirectory =
+            await FilePicker.platform.pickFiles();
+        var file = File("${selectedDirectory!.files.first.path}");
+        contents = await file.readAsString();
+      }
+      setState(() {
+        text = contents!;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> writeSingleFile() async {
+    try {
+      String? contents;
+      await FilePicker.platform.clearTemporaryFiles();
+      await Permission.storage.request();
+      if (await Permission.storage.request().isGranted) {
+        String? selectedDirectory = await FilePicker.platform.getDirectoryPath();
+        //   dialogTitle: 'Please select an output file:',
+        //   fileName: "filetest.txt",
+        //   type: FileType.any,
+        //   allowedExtensions: ['.txt'],
+        //
+        // );
+        if (selectedDirectory != null) {
+
+          var file = File("$selectedDirectory/test_storage.txt");
+          await file.writeAsString(controller.text);
+          // contents = await file.readAsString();
+        }
+
+
+      }
+      setState(() {
+        text = contents!;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Form(
+                key: key,
+                child: TextFormField(
+                  controller: controller,
+                ),
+              ),
+              TextButton(
+                onPressed: () async {
+                  key.currentState!.save();
+                  await writeSingleFile();
+                },
+                child: Text(
+                  "Save to File",
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+              TextButton(
+                onPressed: () async {
+                  await readSingleFile();
+                },
+                child: Text(
+                  "Read File",
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+              Container(
+                child: Text(text),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
